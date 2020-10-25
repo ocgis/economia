48281cc260a730ea86d8d198b934d20deaa02c0f
Christer Gustavsson <cg@nocrew.org>
2020-10-25 13:12:11 +0100

    Added top menu to root page.

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ShowTransaction } from "../components/Transaction"
+import TopMenu from "../components/TopMenu"
 
 export default (
     <Router>
       <Switch>
+        <Route
+          path="/"
+          exact
+          component={TopMenu}
+          />
+
         <Route
           path="/etransactions/:id"
           exact
