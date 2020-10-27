c447843e165ca563a8f3497fa237b36c4c3eff28
Christer Gustavsson <cg@nocrew.org>
2020-10-27 10:54:32 +0100

    Upgrade migrations.

--- db/migrate/20160307123016_create_commodities.rb
+++ db/migrate/20160307123016_create_commodities.rb
@@ -1,4 +1,4 @@
-class CreateCommodities < ActiveRecord::Migration
+class CreateCommodities < ActiveRecord::Migration[4.0]
   def change
     create_table :commodities do |t|
       t.string :space
--- db/migrate/20160308115647_create_accounts.rb
+++ db/migrate/20160308115647_create_accounts.rb
@@ -1,4 +1,4 @@
-class CreateAccounts < ActiveRecord::Migration
+class CreateAccounts < ActiveRecord::Migration[4.0]
   def change
     create_table :accounts do |t|
       t.string :name
