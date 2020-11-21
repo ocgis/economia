7f524736da62e48666dec2061218b77de24112c1
Christer Gustavsson <cg@nocrew.org>
2020-11-21 07:11:42 +0100

    Cleaned script up.

--- parse_gnucash.rb
+++ parse_gnucash.rb
@@ -1,33 +1,7 @@
-def handle_node(node)
-  # if node.element?
-  #   puts "Element"
-  #   puts node.name
-  # elsif node.text?
-  #   puts "Text"
-  #   puts node.content
-  # else
-  #   puts "Unknown, name: #{node.name}"
-  #   puts node.to_s
-  # end
-
-  # if node.name == 'account'
-  #   puts "ACCOUNT"
-  #   puts node
-  #   puts node.xpath('act:name')[0].content
-  # elsif node.name == 'transaction'
-  #   puts "TRANSACTION"
-  #   puts node
-  # else
-  #   puts " #{node.name}"
-  # end
-end
-
 def wash_attribute_name(name)
   washed = name.gsub('-', '_')
   if washed == 'type'
     washed = 'type_'
-#  elsif washed == 'id'
-#    washed = 'id_'
   elsif washed == 'account'
     washed = 'account_id'
   elsif washed == 'parent'
@@ -46,7 +20,6 @@ end
 
 def node_to_db(node, make_object = true)
   node_name = node.name.capitalize
-  # puts "Handling new node #{node_name}"
   attributes = {}
   reference_from = {}
   node.children.each do |xp|
@@ -69,7 +42,6 @@ def node_to_db(node, make_object = true)
       elsif xp.children.size == 0
         # Do nothing
       elsif xp_name[-1] == 's' # List of objects
-        # puts "Handling list of objects #{xp.name}"
         reference_from[xp_name] = []
         xp.children.each do |child|
           if child.element?
@@ -77,13 +49,10 @@ def node_to_db(node, make_object = true)
           end
         end
       else # Single object
-        # puts "Handling single object #{xp.name}"
         child_data = node_to_db(xp, false)
-        # puts child_data.inspect
         child_data[:attributes].each do |key, value|
           attributes[xp_name + '_' + key] = value
         end
-        # puts attributes.inspect
       end
     end
   end
@@ -96,26 +65,11 @@ def node_to_db(node, make_object = true)
   if make_object
     node_name = wash_model_name(node.name)
     model_name = node_name.titleize.delete(' ')
-    # puts model_name
-    # puts attributes.inspect
-    # puts reference_from.inspect
 
     obj = model_name.constantize.new(attributes)
     reference_from.each do |key, vals|
       vals.each do |val|
-        # puts key, val
-        # if val[:attributes].key?("account_id_")
-        #   account_id = val[:attributes]["account_id_"]
-        #   val[:attributes].except!("account_id_")
-        # else
-        #   account_id = nil
-        # end
-
-        child = obj.send(key).build(val[:attributes])
-
-        # if account_id != nil
-        #   child.account = Account.where(id_: account_id)[0]
-        # end
+        obj.send(key).build(val[:attributes])
       end
     end
     return obj
@@ -133,7 +87,6 @@ count = {}
 ['book'].each do |t|
   count[t] = doc.xpath("gnc-v2/gnc:count-data[@cd:type='#{t}']/text()").to_s.to_i
 end
-# puts count.inspect
 
 for i in 1..count['book']
   book = doc.xpath("gnc-v2/gnc:book[#{i}]")
@@ -143,41 +96,17 @@ for i in 1..count['book']
     puts "============================="
     puts "Number of #{t}: #{count}"
 
-#    if t == 'transaction' and count > 100
-#      count = 100
-#    end
     objects = []
     for ti in 1..count
-      # puts "#{t} #{ti}"
       if t == 'price'
         node = book.xpath("gnc:pricedb/price[#{ti}]")
       else
         node = book.xpath("gnc:#{t}[#{ti}]")
       end
-      # puts node
       objects.append(node_to_db(node[0]))
     end
 
     puts "importing #{t} objects"
     wash_model_name(t).titleize.constantize.import objects, recursive: true
   end
-
-  # Account.all.each do |account|
-  #   account.account_parent = Account.where(id_: account.parent)[0]
-  #   account.save
-  # end
 end
-
-
-# puts "================"
-# ['gnc-v2', 'gnc-v2/gnc:count-data', 'gnc-v2/gnc:book'].each do |xp|
-#   puts "Checking #{xp}"
-#   doc.xpath(xp).each do |row|
-#     puts row.name
-#     row.children.each do |child|
-#       handle_node(child)
-#     end
-#   end
-#   puts "Checked #{xp}"
-#   puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-# end
