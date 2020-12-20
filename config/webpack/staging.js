caef8aa351eaf53d0327084c505a6c2cec5da36e
Christer Gustavsson <cg@nocrew.org>
2020-12-20 08:31:52 +0100

    Don't include source maps in staging or production.

--- config/webpack/staging.js
+++ config/webpack/staging.js
@@ -2,4 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'staging'
 
 const environment = require('./environment')
 
+const config = environment.toWebpackConfig()
+config.devtool = 'none'
+
 module.exports = environment.toWebpackConfig()
