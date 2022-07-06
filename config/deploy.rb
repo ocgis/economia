e15248df11a46a00e7b347009f4b606059299b90
Christer Gustavsson <cg@nocrew.org>
2022-07-06 10:34:51 +0200

    Limit the number of kept releases to 2.

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :default_env, { SECRET_KEY_BASE: "347M3" }
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 2
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
