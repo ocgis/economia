c5893464f141e5b9729ddc06e8794e01d7cb815d
Christer Gustavsson <cg@nocrew.org>
2022-08-05 20:54:08 +0200

    Upgraded to react 17.

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "../components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/App';
 
-document.addEventListener("DOMContentLoaded", () => {
-  render(
-    <App />,
-    document.body.appendChild(document.createElement("div"))
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.body.appendChild(document.createElement('div')),
   );
 });
