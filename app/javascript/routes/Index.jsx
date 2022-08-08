203f783dddecbd51ad12423b985ed7d5667e2858
Christer Gustavsson <cg@nocrew.org>
2022-08-08 10:02:18 +0200

    Added "Not found" route.

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Link, Route, Routes,
+} from 'react-router-dom';
 import IndexBook from '../components/IndexBook';
 import ExportBook from '../components/ExportBook';
 import ImportBook from '../components/ImportBook';
@@ -13,6 +15,18 @@ import NewTransaction from '../components/NewTransaction';
 import ShowTransaction from '../components/ShowTransaction';
 import IndexReport from '../components/IndexReport';
 import ShowReport from '../components/ShowReport';
+
+function NotFound() {
+  return (
+    <div>
+      <h1 style={{ color: 'red', fontSize: 100 }}>404</h1>
+      <h3>Page Not Found</h3>
+      <p>
+        <Link to="/">Go Home</Link>
+      </p>
+    </div>
+  );
+}
 
 export default (
   <Router>
@@ -93,6 +107,11 @@ export default (
         path="/books/:bookId/reports"
         exact
         element={<IndexReport />}
+      />
+
+      <Route
+        path="*"
+        element={<NotFound />}
       />
 
     </Routes>
