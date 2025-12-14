6a78ed4e9a7d32f2cb45e28172546716ab63520e
Christer Gustavsson <cg@nocrew.org>
2025-12-14 11:28:30 +0100

    Updated mobile ui for show account

--- app/javascript/mobile/ShowAccount.jsx
+++ app/javascript/mobile/ShowAccount.jsx
@@ -3,7 +3,7 @@ import moment from 'moment';
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Link, useLocation, useNavigate, useParams,
+  useLocation, useNavigate, useParams,
 } from 'react-router-dom';
 import { Grid } from 'antd-mobile/2x';
 import 'antd-mobile/2x/es/global';
@@ -59,7 +59,7 @@ class ShowAccount extends React.Component {
         if (error.response) {
           this.setState({ error: `${error.response.status} ${error.response.statusText}` });
         } else {
-          console.log(error);
+          console.log('ERROR:', error); // eslint-disable-line no-console
         }
       });
   }
@@ -73,24 +73,27 @@ class ShowAccount extends React.Component {
     const goToTransaction = () => navigate(`/m/books/${bookId}/etransactions/${split.etransaction_id}`);
 
     const base = (
-      <Grid columns={24} key={split.id}>
-        <Grid.Item span={5} onClick={goToTransaction}>
-          { moment(split.etransaction.date_posted).format('YYYY-MM-DD') }
-        </Grid.Item>
-        <Grid.Item span={19} onClick={goToTransaction}>
-          { split.etransaction.description }
-        </Grid.Item>
-        <Grid.Item span={18} onClick={goToTransaction}>
-          <div style={{ float: 'right' }}>
-            { Number(split.value).toFixed(2) }
-          </div>
-        </Grid.Item>
-        <Grid.Item span={6} onClick={goToTransaction}>
-          <div style={{ float: 'right' }}>
-            { Number(split.balance).toFixed(2) }
-          </div>
-        </Grid.Item>
-      </Grid>
+      <>
+        <hr />
+        <Grid columns={24} key={split.id}>
+          <Grid.Item span={5} onClick={goToTransaction}>
+            { moment(split.etransaction.date_posted).format('YYYY-MM-DD') }
+          </Grid.Item>
+          <Grid.Item span={19} onClick={goToTransaction}>
+            { split.etransaction.description }
+          </Grid.Item>
+          <Grid.Item span={18} onClick={goToTransaction}>
+            <div style={{ float: 'right' }}>
+              { Number(split.value).toFixed(2) }
+            </div>
+          </Grid.Item>
+          <Grid.Item span={6} onClick={goToTransaction}>
+            <div style={{ float: 'right' }}>
+              { Number(split.balance).toFixed(2) }
+            </div>
+          </Grid.Item>
+        </Grid>
+      </>
     );
 
     switch (split.reconciled_state) {
