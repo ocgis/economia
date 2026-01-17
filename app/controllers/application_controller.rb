d6e9ccae990c4146d9649189c380b3c1a37bed02
Christer Gustavsson <cg@nocrew.org>
2026-01-17 14:54:54 +0100

    Redirect to original url when logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :local_auth
 
   def after_sign_in_path_for(_resource)
-    request.env['omniauth.origin'] || root_path
+    url = session[:fall_back_url]
+    session[:fall_back_url] = nil
+    url = request.env['omniauth.origin'] || root_path if url.nil?
+    url
+  end
+
+  private
+
+  def local_auth
+    session[:fall_back_url] = request.url unless request.url.include?('/users/')
+    authenticate_user!
   end
 end
